9b38a5d43e7ceff7e2199c3d03f2c7330be89aa0
Jesse DeFer <jesse.defer@asu.edu>
2018-01-18 09:36:33 -0700

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/dumb-init /bin/sh
+set -e
+
+# Note above that we run dumb-init as PID 1 in order to reap zombie processes
+# as well as forward signals to all processes in its session. Normally, sh
+# wouldn't do either of these functions so we'd leak zombies as well as do
+# unclean termination of all our sub-processes.
+
+export CT_CONFIG_DIR="${CT_CONFIG_DIR:-/consul-template/config}"
+
+# You can also set the CT_LOCAL_CONFIG environment variable to pass some
+# Consul Template configuration JSON without having to bind any volumes.
+if [ -n "$CT_LOCAL_CONFIG" ]; then
+  echo "$CT_LOCAL_CONFIG" > "$CT_CONFIG_DIR/local-config.hcl"
+fi
+
+if [ -n "$(find "$CT_CONFIG_DIR" -mindepth 1 -maxdepth 1 -type f 2>/dev/null)" ]; then
+  /bin/consul-template -consul-addr="$CONSUL_HOST" -consul-ssl -vault-addr="$VAULT_ADDR" -vault-ssl -vault-renew-token="false" -config="$CT_CONFIG_DIR" -once
+else
+  echo "the directory [$CT_CONFIG_DIR] is empty or non-existent; skipping consul-template configuration"
+fi
+
+exec "$@"
